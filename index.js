54a14f2c68d368f59f9d74ed550cba332aaab3d4
rsnirob <niroborin111@gmail.com>
2023-05-02 12:10:01 +0600

    chef data loaded

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+const categories = require('./data/chefCategories.json');
 
 app.get('/', (req,res) => {
     res.send('Chef is running');
 })
 
-
+app.get('/categories', (req,res) => {
+    res.send(categories);
+})
 
 app.listen(port, () => {
     console.log(`Chef API is running on port: ${port}`);
