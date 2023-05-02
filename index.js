77fab7d9fd336e3e4ebf7cb7d678abb2710a78d8
rsnirob <niroborin111@gmail.com>
2023-05-02 22:51:27 +0600

    vercel config file added

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const express = require('express');
-
 const app = express();
+const cors = require('cors');
 
 const port = process.env.PORT || 5000;
 
-const categories = require('./data/chefCategories.json');
+const chefs = require('./data/chefCategories.json');
+// const chefData = require('.data/chefData.json');
+
+app.use(cors());
 
 app.get('/', (req,res) => {
     res.send('Chef is running');
 })
 
-app.get('/categories', (req,res) => {
-    res.send(categories);
+app.get('/chef', (req, res) => {
+    // console.log(chefs);
+    res.send(chefs);
 })
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedChef = chefs.filter(chef => chef.id === id);
+    res.send(selectedChef);
+})
+
+// app.get('/chefdata', (req, res) => {
+//     // console.log(chefData);
+//     res.send(chefData);
+// })
 
 app.listen(port, () => {
     console.log(`Chef API is running on port: ${port}`);
